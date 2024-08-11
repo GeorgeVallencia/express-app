0c749dfb613f5932b679189fa3e478f75ddab6cd
George Vallencia <georgevallencia@gmail.com>
2024-08-11 11:51:04 +0300

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,14 @@
+// prod.js - production keys here!!
+module.exports ={
+  googleClientID : process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+  // mongoURI: 'mongodb+srv://georgevallencia:JAafE6aCrH9hn1pg@cluster0.xav4lw1.mongodb.net/?retryWrites=true&w=majority&expressdb=Cluster0',
+  //mongodb+srv://valencia:<password>@cluster0.hlqglzf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+
+  //production
+  //prodPassword:JsWsiMWpupsC6s7w,
+  //mongodb+srv://georgevallencia:<password>@cluster2.blvyv.mongodb.net/expressdbprod?retryWrites=true&w=majority&appName=Cluster2
+};
